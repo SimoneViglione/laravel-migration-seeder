a6c2784a5f979d5849106813ea82a81baccf1a5f
Simone Viglione <zimoviglio51@gmail.com>
2023-05-12 16:35:33 +0200

    aggiunta query in PageController per treni odierni

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -9,7 +9,8 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function index() {
-        $trains = Train::all();
+        $today = date('Y-m-d');
+        $trains = Train::where('departure_time', 'LIKE', "$today%")->get();
         return view('home', compact('trains'));
         
     }
