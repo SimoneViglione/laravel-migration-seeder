af767a030b467729365ee70849c1d44733ac605c
Simone Viglione <zimoviglio51@gmail.com>
2023-05-12 15:18:39 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <main>
+        <h1 class="text-light"> Treni </h1>  
+
+        <div class="row">
+
+        </div>
+    </main>
+    
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+
+    <nav class="nav flex-column">
+        <p class="text-center text-light"> Footer bellissimo©  all rights reserved
+    </nav>
+
+</footer>
